0476557311a298fb19ad946d45ca6939ace9fd2a
Vassili Régnier <vassili.regnier@etu.univ-amu.fr>
2025-09-23 14:53:09 +0200

    Add config.example.php with database connection details

---
+++ private/config/config.example.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_HOST", "postgresql-medboard.alwaysdata.net");
+define("DB_USER", "medboard");
+define("DB_PASS", "lazone3954*!");
+define("DB_NAME", "medboard_database");
+define("DB_CHARSET", "utf8mb4");
+?>
