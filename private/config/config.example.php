ad776fb4f40b62ba5bf5c5e58f3aae8e0f166171
medboard <medboard@alwaysdata>
2025-09-25 16:50:05 +0200

    Deleted example config file

--- private/config/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_HOST", "host.example.com");
-define("DB_USER", "username");
-define("DB_PASS", "password");
-define("DB_NAME", "database_name");
-define("DB_CHARSET", "charset_name");
-?>
