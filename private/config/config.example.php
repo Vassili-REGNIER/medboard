dbfa06a6602f2055f1c32d2e890c291d51f713e3
Vassili Régnier <vassili.regnier@etu.univ-amu.fr>
2025-09-25 12:14:37 +0200

    Update .gitignore to include config.example.php and add base paths to config.example.php; remove www/index.php and create home.php with basic HTML structure

--- private/config/config.example.php
+++ private/config/config.example.php
@@ -1,4 +1,11 @@
 <?php
+
+// Chemins de base
+define('BASE_PATH', dirname(__DIR__));
+define('MODULES_PATH', BASE_PATH . '/private/modules');
+
+
+// Config de connexion DB (exemple)
 define("DB_HOST", "host.example.com");
 define("DB_USER", "username");
 define("DB_PASS", "password");
