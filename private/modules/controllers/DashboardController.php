7b38f3716f6e65f89ca627d1753b4006920d93e3
Vassili <vassili.regnier@etu.univ-amu.fr>
2025-10-08 22:25:30 +0200

    Refactor DashboardController to improve handling of session data for user names and specialization

--- private/modules/controllers/DashboardController.php
+++ private/modules/controllers/DashboardController.php
@@ -3,9 +3,9 @@ final class DashboardController
 {
     public function index(): void
     {
-        $prenom = ucfirst($_SESSION["firstname"], ' -' ?? '');
-        $nom = ucfirst($_SESSION["lastname"], ' -' ?? '');
-        $specialization = ucfirst($_SESSION["specialization"], ' -' ?? '');
+        $prenom = isset($_SESSION['firstname']) ? ucfirst($_SESSION['firstname']) : '';
+        $nom = isset($_SESSION['lastname']) ? ucfirst($_SESSION['lastname']) : '';
+        $specialization = isset($_SESSION['specialization']) ? ucfirst($_SESSION['specialization']) : '';
 
         require __DIR__ . '/../views/dashboard.php';
     }
